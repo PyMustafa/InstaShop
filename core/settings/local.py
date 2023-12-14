02998c637781cea36681b9c64b37058e78699646
pyMustafa <proo.eng2018@gmail.com>
2023-12-14 09:21:12 +0200

    Initial commit

---
+++ core/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+import os
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
