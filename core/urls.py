62f7844abb75a46ca620fc59dd1f9d21e84218de
pyMustafa <proo.eng2018@gmail.com>
2023-12-18 10:05:34 +0200

    Primary Templates & static files

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
